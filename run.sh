81840e99fbea3d730d1e5ff4b41e94daf959a0af
goodrainzh <33080353+goodrainzh@users.noreply.github.com>
2018-03-30 15:46:22 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 [ $DEBUG ] && set -x 
-
+START_NGINX=$(START_NGINX:-true)
 
 sleep ${PAUSE:-0}
 
@@ -11,8 +11,10 @@ crond
 # build static
 bundle exec jekyll build
 
-if [ "$START_NGINX" = "true" ];then
 # start nginx
-nginx -t
-nginx -g 'daemon off;'
+if [ "$START_NGINX" == "true" ];then
+  nginx -t
+  nginx -g 'daemon off;'
+else
+  bundle exec jekyll server
 fi
