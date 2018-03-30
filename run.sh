bd14441859d38de075e43665324c8c49e4314039
zhouyq <zyq916@gmail.com>
2018-03-30 19:46:20 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,11 +7,10 @@ sleep ${PAUSE:-0}
 # run crond
 crond
 
-# build static
-bundle exec jekyll build
-
 # start nginx
 if [ "$START_NGINX" == "true" ];then
+  # build static
+  bundle exec jekyll build
   nginx -t
   nginx -g 'daemon off;'
 else
