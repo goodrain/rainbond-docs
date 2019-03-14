d7cd9277ff5d4239c46cb4218d0ae8e7dadb2578
barnett <576501057@qq.com>
2019-03-14 19:03:00 +0800

    change docs domain @deploy

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-BASEURL=${BASE_URL:-"https://v5-1.docs.rainbond.com/docs/v5.1/"}
+BASEURL=${BASE_URL:-"https://www.rainbond.com/docs/"}
 exec hugo server --bind="0.0.0.0" --disableLiveReload=true --baseURL=${BASEURL} --appendPort=false
