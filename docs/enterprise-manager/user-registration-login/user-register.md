daf170140c7c444951110f21a400c362881dde8e
zhangqi <13651281168@163.com>
2022-02-25 17:17:59 +0800

    change some docs

---
+++ docs/enterprise-manager/user-registration-login/user-register.md
@@ -0,0 +1,88 @@
+---
+title: 用户、管理员注册
+description: Rainbond 用户、管理员管理和注册文档说明
+---
+
+### 功能描述
+如果您第一次使用rainbond平台，首先得注册一个账户，以方便您访问rainbond应用控制平台。用户在Rainbond平台中只作为操作身份的标识，不具备给类资源所属权，Rainbond的服务、应用、网关策略、插件等资源只属于团队,用户只能从团队中获取操作权。
+每一个用户可以加入多个团队，主动加入团队时需要团队管理员审核。
+
+### 平台管理员注册
+在Rainbond安装完成后，Rainbond注册的第一个用户是（平台）企业管理员，拥有管理员所拥有的权限，能够对平台的一些基础功能进行设置管理，同时具备对团队的管理权，后续再注册的用户为普通用户，企业管理员用户可以查看当前企业下所有团队的资源信息。
+
+* 企业版中企业管理员可以登录管理后台
+* 企业版中可以通过管理后台添加更多的企业管理员用户
+
+**添加企业管理员**
+
+（平台）企业管理员可在企业视图`设置`界面`企业管理员管理`添加已有用户为企业管理员
+
+
+### 普通用户注册
+
+在开启用户注册功能的情况下，注册流程由用户完成，过程如下：
+
+1. 从用户登录页面点击`注册账户`进入用户注册页面
+
+2. 填写`用户名，邮箱，密码，验证码`等信息提交注册
+
+3. 注册结果反馈
+
+4. 成功后进入`企业视图`页面，选择其中一个团队加入，等待团队管理员审核
+
+5. 审核通过可进入`团队视图`参与相关应用和其他资源的管理
+
+提示: 进入到某一个团队才能拥有团队所拥有的资源的操作权限
+
+> 用户注册页面演示
+
+![注册页面](https://grstatic.oss-cn-shanghai.aliyuncs.com/images/docs/5.2/user-manual/user-registration-login/user-register/register_1.jpg)
+
+![注册页面](https://grstatic.oss-cn-shanghai.aliyuncs.com/images/docs/5.2/user-manual/user-registration-login/user-register/register_6.jpg)
+
+> 注册结果反馈
+
+![注册页面](https://grstatic.oss-cn-shanghai.aliyuncs.com/images/docs/5.2/user-manual/user-registration-login/user-register/register_4.png)
+
+> 企业视图申请加入团队
+
+
+![加入团队](https://grstatic.oss-cn-shanghai.aliyuncs.com/images/docs/5.2/user-manual/user-registration-login/user-register/register_5.jpg)
+
+> 非管理员用户注册完成后必须加入到某个团队才能进入系统；需等待团队管理员在团队视图团队管理审核通过。
+
+![审核](https://grstatic.oss-cn-shanghai.aliyuncs.com/images/docs/5.2/user-manual/user-registration-login/user-register/shenhe.png)
+
+
+### 关闭注册功能
+
+为了适应企业账号管理方式，管理员可以通过`企业视图`-->`设置`关闭用户注册功能，在登录界面将不会再有`注册账户`按钮，采用手动添加用户的方式。
+
+![关闭注册功能](https://grstatic.oss-cn-shanghai.aliyuncs.com/images/docs/5.2/user-manual/user-registration-login/user-register/register_7.jpg)
+
+### 管理员手动添加用户
+
+通过管理员手动添加用户，可直接选择用户所在团队
+
+在企业视图中，点击`用户-->新增-->填写用户信息-->确定-->完成注册`，即可使用注册的新用户登录Rainbond控制台。
+
+![添加用户](https://grstatic.oss-cn-shanghai.aliyuncs.com/images/docs/5.2/user-manual/user-registration-login/user-register/adduser.png)
+
+### 用户管理
+
+* 用户授权
+
+（平台）企业管理员在企业视图 用户名称 一栏，点击 用户名 即可对用户在不同团队中的 角色 进行分配。
+
+![用户授权](https://grstatic.oss-cn-shanghai.aliyuncs.com/docs/5.3/enterprise-manager/user-registration-login/user-register/role_authorization.jpg)
+
+* 用户删除与密码修改
+
+（平台）企业管理员在企业视图`用户-->删除`删除对应的用户，点击`用户-->编辑`对普通用户的密码进行修改。
+
+![编辑用户](https://grstatic.oss-cn-shanghai.aliyuncs.com/images/docs/5.2/user-manual/user-registration-login/user-register/Change%20Password.png)
+
+
+### 与第三方用户体系集成
+
+Rainbond支持Auth2.0等标准规范的第三方平台用户登录，参考 [Auth2.0相关支持文档](/docs/enterprise-manager/user-registration-login/oauth2.0/)
--- sidebars.js
+++ sidebars.js
@@ -2,99 +2,142 @@ const sidebars = {
   // But you can create a sidebar manually
   docs: [
     {
-      type: 'category',
-      label: '快速开始',
+      type: "category",
+      label: "快速开始",
       collapsible: false,
       items: [
-        'quick-start/start',
-        'quick-start/startcopy'
+        "quick-start/start",
+        "quick-start/startcopy"
       ]
     },
     {
-      type: 'category',
-      label: '快速入门',
+      type: "category",
+      label: "快速入门",
       items: [
-        'get-start/get-start'
+        "get-start/get-start"
       ]
     },
     {
-      type: 'category',
-      label: '架构',
+      type: "category",
+      label: "架构",
       items: [
-        'architecture/architecture'
+        "architecture/architecture"
       ]
     },
     {
-      type: 'category',
-      label: '部署服务组件',
+      type: "category",
+      label: "部署服务组件",
       items: [
-        'component-create/component-create'
+        "component-create/component-create"
       ]
     },
     {
-      type: 'category',
-      label: '管理应用与服务组件',
+      type: "category",
+      label: "管理应用与服务组件",
       items: [
-        'user-manual/user-manual'
+        "user-manual/user-manual"
       ]
     },
     {
-      type: 'category',
-      label: '安装与运维',
+      type: "category",
+      label: "安装与运维",
       items: [
-        'user-operations/user-operations'
+        "user-operations/user-operations"
       ]
     },
     {
-      type: 'category',
-      label: '常见场景最佳实践',
+      type: "category",
+      label: "常见场景最佳实践",
       items: [
-        'practices/practices'
+        "practices/practices"
       ]
     },
     {
-      type: 'category',
-      label: '管理企业',
+      type: "category",
+      label: "管理企业",
       items: [
-        'enterprise-manager/enterprise-manager'
+        {
+          type: "category",
+          label: "用户管理",
+          items: [
+            {
+              type: "category",
+              label: "OAuth2.0集成",
+              items: [
+                "enterprise-manager/user-registration-login/oauth2.0/oauth-product-desc",
+                "enterprise-manager/user-registration-login/oauth2.0/oauth-conf",
+                "enterprise-manager/user-registration-login/oauth2.0/oauth-git",
+                "enterprise-manager/user-registration-login/oauth2.0/oauth-create",
+              ]
+            },
+            "enterprise-manager/user-registration-login/reset_admin_user_password",
+            "enterprise-manager/user-registration-login/user-register",
+          ]
+        },
+        {
+          type: "category",
+          label: "企业中台",
+          items: [
+            "enterprise-manager/enterprise/dashboad",
+            {
+              type: "category",
+              label: "应用市场",
+              items: [
+                "enterprise-manager/enterprise/appcenter/desc",
+                "enterprise-manager/enterprise/appcenter/application-template",
+                "enterprise-manager/enterprise/appcenter/share-app-market",
+                "enterprise-manager/enterprise/appcenter/add-app",
+                "enterprise-manager/enterprise/appcenter/app-specification",
+              ]
+            },
+            {
+              type: "category",
+              label: "团队管理",
+              items: [
+                "enterprise-manager/enterprise/teams/create-team",
+                
+              ]
+            },
+          ]
+        }
       ]
     },
     {
-      type: 'category',
-      label: '版本升级',
+      type: "category",
+      label: "版本升级",
       items: [
-        'upgrade/5.2.2-5.3.1',
-        'upgrade/5.3.0-upgrade',
-        'upgrade/5.3.1-upgrade',
-        'upgrade/5.3.2-upgrade',
-        'upgrade/5.4.0-upgrade',
-        'upgrade/5.5.0-upgrade'
+        "upgrade/5.2.2-5.3.1",
+        "upgrade/5.3.0-upgrade",
+        "upgrade/5.3.1-upgrade",
+        "upgrade/5.3.2-upgrade",
+        "upgrade/5.4.0-upgrade",
+        "upgrade/5.5.0-upgrade"
       ]
     },
     {
-      type: 'category',
-      label: '社区',
+      type: "category",
+      label: "社区",
       items: [
-        'community/FAQs/faqs',
-        'community/contribution/contribution',
-        'community/upcoming-events',
-        'community/doc-style',
-        'community/change/5.3.0-5.3.1',
-        'community/change/5.3.1-5.3.2',
-        'community/change/5.3.2-5.3.3',
-        'community/change/5.3.3-5.4.0',
-        'community/change/5.4.0-5.5.0'
+        "community/FAQs/faqs",
+        "community/contribution/contribution",
+        "community/upcoming-events",
+        "community/doc-style",
+        "community/change/5.3.0-5.3.1",
+        "community/change/5.3.1-5.3.2",
+        "community/change/5.3.2-5.3.3",
+        "community/change/5.3.3-5.4.0",
+        "community/change/5.4.0-5.5.0"
       ]
     },
     {
-      type: 'category',
-      label: '开源应用精选专区',
+      type: "category",
+      label: "开源应用精选专区",
       items: [
-        'opensource-app/itest',
-        'opensource-app/maxkey',
-        'opensource-app/TDengine',
-        'opensource-app/chatopera',
-        'opensource-app/logikm'
+        "opensource-app/itest",
+        "opensource-app/maxkey",
+        "opensource-app/TDengine",
+        "opensource-app/chatopera",
+        "opensource-app/logikm"
       ]
     }
   ]
