d38e5da44a68887577266d2ef463fa05e7fee95d
zhangqi <13651281168@163.com>
2022-02-25 16:48:06 +0800

    change some docs

--- sidebars.js
+++ sidebars.js
@@ -63,21 +63,38 @@ const sidebars = {
       type: 'category',
       label: '版本升级',
       items: [
-        'upgrade/upgrade'
+        'upgrade/5.2.2-5.3.1',
+        'upgrade/5.3.0-upgrade',
+        'upgrade/5.3.1-upgrade',
+        'upgrade/5.3.2-upgrade',
+        'upgrade/5.4.0-upgrade',
+        'upgrade/5.5.0-upgrade'
       ]
     },
     {
       type: 'category',
       label: '社区',
       items: [
-        'community/community'
+        'community/FAQs/faqs',
+        'community/contribution/contribution',
+        'community/upcoming-events',
+        'community/doc-style',
+        'community/change/5.3.0-5.3.1',
+        'community/change/5.3.1-5.3.2',
+        'community/change/5.3.2-5.3.3',
+        'community/change/5.3.3-5.4.0',
+        'community/change/5.4.0-5.5.0'
       ]
     },
     {
       type: 'category',
       label: '开源应用精选专区',
       items: [
-        'opensource-app/opensource-app'
+        'opensource-app/itest',
+        'opensource-app/maxkey',
+        'opensource-app/TDengine',
+        'opensource-app/chatopera',
+        'opensource-app/logikm'
       ]
     }
   ]
