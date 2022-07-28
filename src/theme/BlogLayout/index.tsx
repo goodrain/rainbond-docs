903e77046b0f9877dcd92e990b44c1a00d1ccd60
Qi Zhang <smallqi1@163.com>
2022-07-28 15:35:04 +0800

    fix: blog style fail
    Signed-off-by: Qi Zhang <smallqi1@163.com>

--- src/theme/BlogLayout/index.tsx
+++ src/theme/BlogLayout/index.tsx
@@ -16,8 +16,7 @@ import { useLocation } from 'react-router-dom'
 export default function BlogLayout(props: Props): JSX.Element {
   const { sidebar, toc, children, ...layoutProps } = props;
   const hasSidebar = sidebar && sidebar.items.length > 0;
-  const location = useLocation();
-  const location_url = location.pathname;
+  const location_url = useLocation().pathname;
   const { title } = sidebar;
   const isBlogPage = location_url.includes('/blog');
   return (
@@ -26,9 +25,9 @@ export default function BlogLayout(props: Props): JSX.Element {
         [styles.blogLayout]: isBlogPage,
         }
       )}>
-        {title === '使用场景' && location_url === '/usescene' || location_url === '/usescene/' ? (
+        {title === '使用场景' && location_url.includes('usescene') ? (
           <h1 className={styles.title}>使用场景</h1>
-        ) : title === '案例' && location_url === '/case' || location_url === '/case/' ? (
+        ) : title === '案例' && location_url.includes('case') ? (
           <h1 className={styles.title}>案例</h1>
         ) : (
           ''
@@ -44,7 +43,7 @@ export default function BlogLayout(props: Props): JSX.Element {
               'col--7': hasSidebar && title === '所有文章',
               'col--9 col--offset-1': !hasSidebar
             })} ${
-              (location_url === '/case' || location_url === '/usescene' || location_url === '/case/' || location_url === '/usescene/') &&
+              (location_url.includes('usescene') || location_url.includes('case')) &&
               styles.container_box
             }`}
             itemScope
