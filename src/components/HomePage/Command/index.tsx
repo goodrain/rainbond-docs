f2fe0d43a9de3388b8955b89d76213b63c90d4e9
Qi Zhang <smallqi1@163.com>
2022-10-07 23:42:59 +0800

    feature: modify allinone command
    Signed-off-by: Qi Zhang <smallqi1@163.com>

--- src/components/HomePage/Command/index.tsx
+++ src/components/HomePage/Command/index.tsx
@@ -30,6 +30,8 @@ export default function Command() {
           <h3>在 Linux 上快速安装体验 Rainbond</h3>
           <CodeBlock language="bash">
         {`docker run --privileged -d -p 7070:7070 -p 80:80 -p 443:443 -p 6060:6060 -p 8443:8443 \\
+--name=rainbond-allinone --restart=on-failure \\
+-v ~/.ssh:/root/.ssh \\
 -v ~/rainbonddata:/app/data \\
 -v /opt/rainbond:/opt/rainbond \\
 -v ~/dockerdata:/var/lib/docker \\
@@ -37,10 +39,12 @@ export default function Command() {
 registry.cn-hangzhou.aliyuncs.com/goodrain/rainbond:v5.8.1-dind-allinone`}
           </CodeBlock>
         </TabPane>
-        <TabPane tab="Mac M1" itemKey="2">
-          <h3>在 Mac M1 上快速安装体验 Rainbond</h3>
+        <TabPane tab="Linux ARM" itemKey="2">
+          <h3>在 Linux ARM 上快速安装体验 Rainbond</h3>
           <CodeBlock language="bash">
         {`docker run --privileged -d -p 7070:7070 -p 80:80 -p 443:443 -p 6060:6060 -p 8443:8443 \\
+--name=rainbond-allinone --restart=on-failure \\
+-v ~/.ssh:/root/.ssh \\
 -v ~/rainbonddata:/app/data \\
 -v /opt/rainbond:/opt/rainbond \\
 -v ~/dockerdata:/var/lib/docker \\
@@ -48,13 +52,38 @@ registry.cn-hangzhou.aliyuncs.com/goodrain/rainbond:v5.8.1-dind-allinone`}
 registry.cn-hangzhou.aliyuncs.com/goodrain/rainbond:v5.8.1-arm64-dind-allinone`}
           </CodeBlock>
         </TabPane>
-        <TabPane tab="Win" itemKey="3">
+        <TabPane tab="Mac Intel" itemKey="3">
+          <h3>在 Mac Intel 上快速安装体验 Rainbond</h3>
+          <CodeBlock language="bash">
+        {`docker run --privileged -d -p 7070:7070 -p 80:80 -p 443:443 -p 6060:6060 -p 8443:8443 \\
+--name=rainbond-allinone --restart=on-failure \\
+-v ~/.ssh:/root/.ssh \\
+-v ~/opt/rainbond:/opt/rainbond \\
+-v ~/rainbonddata:/app/data \\
+-e EIP=<YOUR_IP> \\
+registry.cn-hangzhou.aliyuncs.com/goodrain/rainbond:v5.8.1-dind-allinone`}
+          </CodeBlock>
+        </TabPane>
+        <TabPane tab="Mac M1" itemKey="4">
+          <h3>在 Mac M1 上快速安装体验 Rainbond</h3>
+          <CodeBlock language="bash">
+        {`docker run --privileged -d -p 7070:7070 -p 80:80 -p 443:443 -p 6060:6060 -p 8443:8443 \\
+--name=rainbond-allinone --restart=on-failure \\
+-v ~/.ssh:/root/.ssh \\
+-v ~/opt/rainbond:/opt/rainbond \\
+-v ~/rainbonddata:/app/data \\
+-e EIP=<YOUR_IP> \\
+registry.cn-hangzhou.aliyuncs.com/goodrain/rainbond:v5.8.1-arm64-dind-allinone`}
+          </CodeBlock>
+        </TabPane>
+        <TabPane tab="Win" itemKey="5">
           <h3>在 Win 上快速安装体验 Rainbond</h3>
           <CodeBlock language="bash">
-        {`docker run --privileged -d -p 7070:7070 -p 80:80 -p 443:443 -p 6060:6060 -p 8443:8443 ^
+        {`docker run --privileged -d  -p 7070:7070 -p 80:80 -p 443:443 -p 6060:6060 -p 8443:8443 ^
+--name=rainbond-allinone --restart=on-failure ^
+-v ~/.ssh:/root/.ssh ^
 -v ~/rainbonddata:/app/data ^
--v /opt/rainbond:/opt/rainbond ^
--v ~/dockerdata:/var/lib/docker ^
+-v ~/opt/rainbond:/opt/rainbond ^
 -e EIP=<YOUR_IP> ^
 registry.cn-hangzhou.aliyuncs.com/goodrain/rainbond:v5.8.1-dind-allinone`}
           </CodeBlock>
