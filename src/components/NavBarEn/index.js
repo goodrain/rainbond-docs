1ff64f3a6ed135045510a28830b189a6df6719fc
yangk <yangk@goodrain.com>
2025-04-09 12:13:24 +0800

    perf: modified jump link

--- src/components/NavBarEn/index.js
+++ src/components/NavBarEn/index.js
@@ -26,11 +26,10 @@ export default function Index(props) {
           <div className={styles.nav_container}>
             <ul className={styles.nav_lists}>
               <li>
-                <Link to="/docs/">
-                  <Translate id='navbar.doc'>
-                    Docs
-                  </Translate>
-                </Link>
+                <a
+                    href='https://www.rainbond.com/en/docs'
+                    target='_blank'
+                >Docs</a>
               </li>
               <li>
                 <a
