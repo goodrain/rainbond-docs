dc2fd2454ee151913237cf849ba3c208926680f8
rainbond <xzl17839704765@163.com>
2025-05-21 18:54:51 +0800

    fix: The problem with the drop-down box in the navigation bar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,12 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Link from '@docusaurus/Link';
 import styles from './styles.module.css';
-import clsx from 'clsx';
-import { Button, Dropdown, HotKeys } from '@douyinfe/semi-ui';
+import { Button } from '@douyinfe/semi-ui';
 import { IconFile, IconGallery, IconBox, IconForward, IconGithubLogo, IconLayers, IconSetting, IconApartment, IconApps } from '@douyinfe/semi-icons';
 
 export default function Navbar() {
-
   return (
     <nav className={styles.navbar}>
       <Link href="/" className={styles.homeLink}>
@@ -24,75 +22,57 @@ export default function Navbar() {
           </Link>
         </div>
         <div className={styles.navLink}>
-          <Dropdown
-            position="bottomLeft"
-            render={
-              <Dropdown.Menu>
-                <Link to="/xinchuang" className={styles.dropdownItem}>
-                  <Dropdown.Item>
-                    信创
-                  </Dropdown.Item>
-                </Link>
-                <Link to="/feature/devops" className={styles.dropdownItem}>
-                  <Dropdown.Item>
-                    一体化DevOps
-                  </Dropdown.Item>
-                </Link>
-                <Link to="/feature/multi-cluster" className={styles.dropdownItem}>
-                  <Dropdown.Item>
-                    K8s 多集群管理
-                  </Dropdown.Item>
-                </Link>
-                <Link to="/feature/service-mesh" className={styles.dropdownItem}>
-                  <Dropdown.Item>
-                    开箱即用的微服务治理
-                  </Dropdown.Item>                   
-                </Link>  
-                <Link to="/feature/app-ops" className={styles.dropdownItem}>
-                  <Dropdown.Item>
-                    自动化应用运维
-                  </Dropdown.Item>                   
-                </Link>                   
-              </Dropdown.Menu>
-            }
+          <div className={styles.dropdown}>
+            <Button 
+              theme="borderless" 
+              type="tertiary" 
+              icon={<IconGallery />}
             >
-            <Button theme="borderless" type="tertiary" icon={<IconGallery />}>
               功能特性
             </Button>
-          </Dropdown>
+            <div className={styles.dropdownMenu}>
+              <Link to="/xinchuang" className={styles.dropdownItem}>
+                信创
+              </Link>
+              <Link to="/feature/devops" className={styles.dropdownItem}>
+                一体化DevOps
+              </Link>
+              <Link to="/feature/multi-cluster" className={styles.dropdownItem}>
+                K8s 多集群管理
+              </Link>
+              <Link to="/feature/service-mesh" className={styles.dropdownItem}>
+                开箱即用的微服务治理
+              </Link>
+              <Link to="/feature/app-ops" className={styles.dropdownItem}>
+                自动化应用运维
+              </Link>
+            </div>
+          </div>
         </div>
         <div className={styles.navLink}>
-          <Dropdown
-            position="bottomLeft"
-            render={
-              <Dropdown.Menu>
-                <Link to="/usescene" className={styles.dropdownItem}>
-                  <Dropdown.Item>
-                    使用场景
-                  </Dropdown.Item>
-                </Link>
-                <Link to="/case" className={styles.dropdownItem}>
-                  <Dropdown.Item>
-                    用户案例
-                  </Dropdown.Item>
-                </Link>
-                <Link to="/blog" className={styles.dropdownItem}>
-                  <Dropdown.Item>
-                    博客
-                  </Dropdown.Item>
-                </Link>
-                <Link to="/docs/Intro" className={styles.dropdownItem}>
-                  <Dropdown.Item>
-                    OpenAPI
-                  </Dropdown.Item>                   
-                </Link>
-              </Dropdown.Menu>
-            }
+          <div className={styles.dropdown}>
+            <Button 
+              theme="borderless" 
+              type="tertiary" 
+              icon={<IconLayers />}
             >
-            <Button theme="borderless" type="tertiary" icon={<IconLayers />}>
               深入
             </Button>
-          </Dropdown>
+            <div className={styles.dropdownMenu}>
+              <Link to="/usescene" className={styles.dropdownItem}>
+                使用场景
+              </Link>
+              <Link to="/case" className={styles.dropdownItem}>
+                用户案例
+              </Link>
+              <Link to="/blog" className={styles.dropdownItem}>
+                博客
+              </Link>
+              <Link to="/docs/Intro" className={styles.dropdownItem}>
+                OpenAPI
+              </Link>
+            </div>
+          </div>
         </div>
         <div className={styles.navLink}>
           <Link to="/enterprise_server">
@@ -109,10 +89,10 @@ export default function Navbar() {
           </Link>
         </div>
         <a
-        href="https://github.com/goodrain/rainbond" 
-        target="_blank" 
-        rel="noopener noreferrer"
-        className={styles.github_button}
+          href="https://github.com/goodrain/rainbond" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className={styles.github_button}
         >
           <IconGithubLogo className={styles.github_icon} size="large"/>
         </a>
