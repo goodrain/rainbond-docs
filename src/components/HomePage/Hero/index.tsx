6d6673d4af0e097d31a53af23476d57d58b9a5b9
Qi Zhang <smallqi1@163.com>
2026-03-09 17:32:43 +0800

    feat: Reconstruct the homepage
    Signed-off-by: Qi Zhang <smallqi1@163.com>

--- src/components/HomePage/Hero/index.tsx
+++ src/components/HomePage/Hero/index.tsx
@@ -52,15 +52,15 @@ export default function Home() {
         <div className={styles.hero_stats_row}>
           <div className={styles.hero_stat_item}>
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path><path d="M9 18c-4.51 2-5-2-7-2"></path></svg>
-            <span>Github star 5k+</span>
+            <span>Github star 6k+</span>
           </div>
           <div className={styles.hero_stat_item}>
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>
-            <span>下载安装 1M+</span>
+            <span>下载安装 10M+</span>
           </div>
           <div className={styles.hero_stat_item}>
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
-            <span>生产用户 5000+</span>
+            <span>生产用户 10000+</span>
           </div>
         </div>
       </div>
